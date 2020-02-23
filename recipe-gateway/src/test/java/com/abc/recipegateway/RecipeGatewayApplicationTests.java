26859e6bde6f0bf950cb9b0c6a09ec7a0c0d9ec5
Alhagie Bai Cham <alhagiebcham.ee@gmail.com>
2020-02-22 19:59:54 -0500

    adding recipe-gateway service

---
+++ recipe-gateway/src/test/java/com/abc/recipegateway/RecipeGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abc.recipegateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
