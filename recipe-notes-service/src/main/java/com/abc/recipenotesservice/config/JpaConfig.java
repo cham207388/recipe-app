d008de6c90b972208d5a3530ec0198bb5179bdaa
Alhagie Bai Cham <alhagiebcham.ee@gmail.com>
2020-03-08 21:30:22 -0400

    as of now, rest template load balancing is not working properly

--- recipe-notes-service/src/main/java/com/abc/recipenotesservice/config/JpaConfig.java
+++ recipe-notes-service/src/main/java/com/abc/recipenotesservice/config/JpaConfig.java
@@ -1,11 +1,10 @@
-/*
-package com.abc.recipemainservice.config;
+package com.abc.recipenotesservice.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
-@EnableJpaRepositories(basePackages = "com.abc.recipemainservice")
+@EnableJpaRepositories(basePackages = "com.abc.recipenotesservice")
 public class JpaConfig {
 
-}*/
+}
